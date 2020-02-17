618aa0bdae96b4b4d747109cdb8bbf9516a328de
Mutugi Mutuma <mutugimutuma@gmail.com>
2020-02-18 00:36:52 +0300

    Add backend validation

--- app/auth/views.py
+++ app/auth/views.py
@@ -11,36 +11,64 @@ def register():
     '''
     Function to register new users
     '''
+    errors = []
     if current_user.is_authenticated:
         return redirect('main.index')
-    reg_form = RegisterForm()
-    if reg_form.validate_on_submit():
-        user = User(email = reg_form.email.data, username = reg_form.username.data, password = reg_form.password.data)
-        db.session.add(user)
-        db.session.commit()
+    if request.method == 'POST':
+        reg_form = request.form
+        name = reg_form.get('username')
+        if not name:
+            errors.append('Username must be entered')
+            return render_template('auth/register.html', errors = errors)
+        passw = reg_form.get('password')
+        if not passw:
+            errors.append('Password must be entered')
+            return render_template('auth/register.html', errors = errors)
+        pass_confirm = reg_form.get('password_confirmation')
+        if not pass_confirm:
+            errors.append('Password confirmatio must be entered')
+            return render_template('auth/register.html', errors = errors)
+        if pass_confirm != password:
+            errors.append('Passwords Must match')
+            return render_template('auth/register.html', errors = errors)
+        form = RegisterForm()
+        if form.validate_on_submit():
+            user = User(email = form.email.data, username = form.username.data)
+            user.set_password(password)
+            user.save_user()
         
-        mail_message('Welcome to Pitch','email/welcome',user.email,user=user)
         return redirect(url_for('auth.login'))
         flash('Thank you for signing up!')
 
-    return render_template('auth/register.html', form = reg_form)
+    return render_template('auth/register.html', form = form)
 
 @auth.route('/login', methods = ['GET', 'POST'])
 def login():
     '''
     Function to deal with user login 
     '''
+    errors = []
     if current_user.is_authenticated:
         return redirect('main.index')
-    login_form = LoginForm()
-    if login_form.validate_on_submit():
-        user = User.query.filter_by(email = login_form.email.data).first()
-        if user is not None and user.verify_password(login_form.password.data):
-            login_user(user, login_form.remember.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
-        flash('Invalid username or Password')
+    if request.method == 'POST':
+        login_form = request.form
+        name = login_form.get('username')
+        if not name:
+            errors.append('Username must be entered')
+            return render_template('auth/login.html', errors = errors)
+        passw = login_form.get('password')
+        if not passw:
+            errors.append('Password must be entered')
+            return render_template('auth/login.html', errors = errors)
+        form = LoginForm()
+        if form.validate_on_submit():
+            user = User.query.filter_by(email = form.email.data).first()
+            if user is not None and user.verify_password(form.password.data):
+                login_user(user, form.remember.data)
+                return redirect(request.args.get('next') or url_for('main.index'))
+            flash('Invalid username or Password')
 
-    return render_template('auth/login.html', form = login_form)
+    return render_template('auth/login.html', form = form)
 
 
 @auth.route('/logout')
