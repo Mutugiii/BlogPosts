769c2d5eee74beecb65c98079afba3b3f1a804aa
Mutugi Mutuma <mutugimutuma@gmail.com>
2020-02-17 23:57:13 +0300

    Implement auth views | Auth blueprint

---
+++ app/auth/forms.py
@@ -0,0 +1,46 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField, SubmitField, BooleanField, PasswordField, ValidationError
+from wtforms.validators import Required, Email , EqualTo, Length
+from ..models import User
+
+class RegisterForm(FlaskForm):
+    '''
+    Form class to implement the user Registration form
+    Args:
+        FlaskForm: class from wtforms
+    '''
+    email = StringField('Email Address', validators = [Required(),Email()])
+    username = StringField('Username', validators = [Required()])
+    password = PasswordField('Password', validators = [Required(), EqualTo('password_confirmation', message= 'Passwords must match'), Length(min=6)])
+    password_confirmation = PasswordField('Confirm Password', validators = [Required(), Length(min=6)])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        '''
+        Custom validator for user email
+        '''
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError('There is already an account with that email!')
+
+    def validate_username(self,data_field):
+        '''
+        Custom validator for username
+        '''
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is already taken!')
+        
+class LoginForm(FlaskForm):
+    '''
+    Class to implement user login form
+    '''
+    email = StringField('Email Address', validators = [Required(), Email()])
+    password = PasswordField('Password', validators = [Required()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Log in')
+
+class UpdateProfileForm(FlaskForm):
+    '''
+    Class to implement update profile form
+    '''
+    bio = TextAreaField('Tell us about yourself!', validators=[Required()])
+    submit = SubmitField('Update Profile')
---
+++ app/auth/views.py
@@ -0,0 +1,50 @@
+from flask import render_template, url_for, redirect, flash, request
+from . import auth
+from .forms import RegisterForm,LoginForm
+from ..models import User
+from flask_login import login_user,logout_user,login_required, current_user
+from ..email import mail_message
+from .. import db
+
+@auth.route('/register', methods = ['GET','POST'])
+def register():
+    '''
+    Function to register new users
+    '''
+    if current_user.is_authenticated:
+        return redirect('main.index')
+    reg_form = RegisterForm()
+    if reg_form.validate_on_submit():
+        user = User(email = reg_form.email.data, username = reg_form.username.data, password = reg_form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        
+        mail_message('Welcome to Pitch','email/welcome',user.email,user=user)
+        return redirect(url_for('auth.login'))
+        flash('Thank you for signing up!')
+
+    return render_template('auth/register.html', form = reg_form)
+
+@auth.route('/login', methods = ['GET', 'POST'])
+def login():
+    '''
+    Function to deal with user login 
+    '''
+    if current_user.is_authenticated:
+        return redirect('main.index')
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or Password')
+
+    return render_template('auth/login.html', form = login_form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return render_template('index.html')
