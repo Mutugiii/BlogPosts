dfa12624061e46d4073a9c86c93eea85a1f66cae
Mutugi Mutuma <mutugimutuma@gmail.com>
2020-02-18 14:49:01 +0300

    Make api call | Display in templates

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,11 @@
 from . import main
 from flask import render_template
+from ..requests import get_quote
 
 @main.route('/')
 def index():
     '''
     Main index route
     '''
-    return render_template('index.html')
+    quotes = get_quote()
+    return render_template('index.html', quotes = quotes)
